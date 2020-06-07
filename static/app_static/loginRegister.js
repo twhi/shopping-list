a87610154e4cbb14bae3ae6588f8487226367153
Tom <twhitehead.1991@gmail.com>
2020-06-07 18:17:41 +0100

    Deployment

---
+++ static/app_static/loginRegister.js
@@ -0,0 +1,55 @@
+// submit login form to server via AJAX
+$(document).on('submit', '#login', function (e) {
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data: {
+            username: $("#login-user").val(),
+            password: $("#login-pass").val(),
+            requestType: 'login',
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+            action: 'post'
+        },
+        success: function (response) {
+            if (response.redirect_url) {
+                window.location.href = response.redirect_url;
+                return
+            }
+        },
+        error: function (data) {
+            $("#login-error-message").text(data.responseJSON.error);
+            $("#login-error-message").addClass("alert alert-danger");
+        },
+    });
+});
+
+// submit registration form to server via AJAX
+$(document).on('submit', '#registration', function (e) {
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data: {
+            email: $("#reg-email").val(),
+            first_name: $("#reg-first-name").val(),
+            last_name: $("#reg-last-name").val(),
+            password1: $("#reg-pass-1").val(),
+            password2: $("#reg-pass-2").val(),
+            requestType: 'registration',
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+            action: 'post'
+        },
+        success: function (response) {
+            if (response.redirect_url) {
+                window.location.href = response.redirect_url;
+                return
+            }
+        },
+        error: function (data) {
+            $("#reg-error-message").empty();
+            for (e in data.responseJSON) {
+                $("#reg-error-message").append(data.responseJSON[e][0].message, '<br>');
+            }
+            $("#reg-error-message").addClass("alert alert-danger");
+        },
+    });
+});
